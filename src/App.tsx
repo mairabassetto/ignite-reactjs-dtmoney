adff8e2919d17fd34d16e6746f461cb345e06e64
mairabassetto <mairabassetto@gmail.com>
2021-11-06 17:53:00 -0300

    Add useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import Modal from "react-modal";
-import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./TransactionsContext";
+
+import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
 
@@ -18,7 +20,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -29,6 +31,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </ TransactionsProvider>
   );
 }
